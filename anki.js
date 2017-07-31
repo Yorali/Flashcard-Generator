7659b4663e246fd028ade5312b51f5a71ddf7e51
Ajibola Ayeni <hideayeni@gmail.com>
2017-07-30 20:45:28 -0500

    Delete anki.js

--- anki.js
+++
@@ -1,8 +0,0 @@
-var word = process.argv[2];
-var sentence = process.argv [3];
-
-var basic = require("./BasicCard.js");
-var cloze = require("./ClozeCard.js");
-
-var leda = new cloze.ClozeCard("Leda is the guitarist of Far East Dizain", "Leda");
-var leda = new BasicCard("Leda is the guitarist of Far East Dizain", "Leda");
